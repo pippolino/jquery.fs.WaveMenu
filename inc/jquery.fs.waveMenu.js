f1ee0b09b51b7d489dc44fa448637745a5dab0cb
Federico Soldani <pippo@daemon-ware.com>
2012-04-19 16:00:14 +0200

    Fix hidden menu Fix webkit images without dimensions

--- inc/jquery.fs.waveMenu.js
+++ inc/jquery.fs.waveMenu.js
@@ -1,24 +1,30 @@
-/*
-    Options:
-    scale: scale for element when mouse over (default is 120)
-    beginningScale: beginning scale for all elements (default is 100)
-    neighborsScale: scale for mouse over element's neighbors (default is the beginning scale)
-    rememberSelected: if true remeber last selected element (default true)
-    defaultSelectedClass: class for default element selected
-
-    TODO:
-    - callbackFunction
-    - hidden menu
-    - images without dimension not work on chrome and safari
- */
-//Optional parameter includeMargin is used when calculating outer dimensions
-(function($) {
+/*!
+* jQuery waveMenu plugin v1.2.1
+* http://github.com/pippolino/jquery.fs.WaveMenu
+*
+*    Usage:
+*    $('#menu').fsWaveMenu({scale: 100, beginningScale: 70});
+*
+*    Options:
+*    scale: scale for element when mouse over (default is 120)
+*    beginningScale: beginning scale for all elements (default is 100)
+*    neighborsScale: scale for mouse over element's neighbors (default is the beginning scale)
+*    rememberSelected: if true remeber last selected element (default true)
+*    defaultSelectedClass: class for default element selected
+*
+*    webkit fix from Paul Irish. thx!
+*    hidden menu fix from ben-lin (ben). tnx!
+*
+*    TODO:
+*    - callbackFunction
+*/
+;(function($, undefined) {
+'use strict';
 
 	//Wave Menu Animation
 	function FsWaveMenu(el, options) {
         this.name = "jquery.fs.WaveMenu";
-        this.version = "1.1.1";
-        this.author = "Federico Soldani",
+        this.version = "1.2.1";
 
 		//Defaults:
 		this.defaults = {
@@ -35,6 +41,9 @@
 
 		//Privates:
 		this.$el = $(el);
+        this.imagesReady = false;
+
+        this.BLANK = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
 	}
 
 	// Separate functionality from object creation
@@ -44,14 +53,24 @@
 			var self = this;
             var isBeginningScaled = self.opts.beginningScale == 100;
 
-            // Selected Manage
+            // Hide menu if intial scale is different than 100s
+            if(!isBeginningScaled) {
+                self.$el.hide();
+            }
+
+            // Preload Images if Need for calc dimensions
+            if(!self.imagesReady) {
+                self.preloadImages();
+                return;
+            }
+
+            // Scale item selected when leave the menu
             if(self.opts.rememberSelected) {
-                self.$el.hover(function() {
-                    self.restoreScaleElementAndChildren($("[waveSelected]", self.$el).removeClass("waveMenuSelected"), {stop: true});
-                }, function() {
+                self.$el.mouseleave(function() {
                     self.scaleElementAndChildren($("[waveSelected]", self.$el).addClass("waveMenuSelected"), {stop: true});
                 });
             }
+
 
             self.$el.children().each(function() {
                 var kid = $(this);
@@ -66,16 +85,31 @@
                         $(this).attr("waveSelected", "true");
                     });
                 }
+
                 // Add Hover Event
-                kid.hover(function() {
+                kid.mouseenter(function() {
+                    // First restore selected
+                    if(self.opts.rememberSelected) {
+                        var item = $("[waveSelected]", self.$el).removeClass("waveMenuSelected");
+                        if(kid.get(0) != item.get(0)) {
+                            self.restoreScaleElementAndChildren(item, {stop: true});
+                        }
+                    }
+
+                    // Scale item on hover
                     self.scaleElementAndChildren(kid.addClass("waveMenuOver"), {stop: true});
                     // Scale neighbors
                     if( self.opts.neighborsScale != self.opts.beginningScale) {
                         self.scaleElementAndChildren(kid.prev().addClass("waveMenuNeighborsOver"), {stop: true, scale: self.opts.neighborsScale});
                         self.scaleElementAndChildren(kid.next().addClass("waveMenuNeighborsOver"), {stop: true, scale: self.opts.neighborsScale});
                     }
-                }, function() {
-                    self.restoreScaleElementAndChildren(kid.removeClass("waveMenuOver"), {stop: true});
+                });
+                kid.mouseleave(function() {
+                    kid.removeClass("waveMenuOver");
+                    if(!self.opts.rememberSelected || (self.opts.rememberSelected && !kid.attr("waveSelected"))) {
+                        self.restoreScaleElementAndChildren(kid, {stop: true});
+                    }
+
                     // Restore neighbors
                     if( self.opts.neighborsScale != self.opts.beginningScale) {
                         self.restoreScaleElementAndChildren(kid.prev().removeClass("waveMenuNeighborsOver"), {stop: true});
@@ -89,13 +123,24 @@
                 self.scaleElementAndChildren($("." + self.opts.defaultSelectedClass, self.$el).attr("waveSelected", "true").addClass("waveMenuSelected"), {stop: true});
             }
 
-            // Show Menu if hidden only if beginning scale is 100%
-            // otherwise is show in resize callback function
-            //if(isBeginningScaled) {
-            if(true) {
-                self.$el.fadeIn();
-            }
+            // Show menu if hidden
+            self.$el.fadeIn();
 		},
+
+        preloadImages: function() {
+            var self = this;
+            if(!self.imagesReady) {
+               self.imagesLoaded($("img", self.$el), function($images, $proper, $broken) {
+                    $images.each(function() {
+                        //var dim = {width: this.width, height: this.height}; // Note: $(this).width() will not work for in memory images.
+                        var dim = {width: self.actual($(this), 'width' ), height: self.actual($(this), 'height' )};
+                        self.getOriginalDimension(this, {force: true, dimension: dim});
+                    });
+                    self.imagesReady = true;
+                    self.init();
+                });
+            }
+        },
 
         resize: function(el, scale, opt, callback) {
             var self = $(el);
@@ -116,8 +161,7 @@
             var self = this;
             var scale = opt.scale ? opt.scale : false;
             var stop = opt.stop ? opt.stop : false;
-            var duration = opt.duration ? {duration: opt.duration} : {};
-            //var stop = false;
+            var duration = (opt.duration === undefined) ? {} : {duration: opt.duration};
             self.scale(stop ? el.stop(true, true) : el, scale, duration).children().each(function() {
                 self.scale(stop ? $(this).stop(true, true) : $(this), scale, duration);
             });
@@ -130,14 +174,15 @@
             return this.resize(self, scale ? scale : this.opts.scale, opt, callback);
         },
 
-        getOriginalDimension: function(el) {
+        getOriginalDimension: function(el, opt) {
+            var opt =  $.extend({}, {}, opt);
             var self = $(el);
             var oriObj = self.data("waveMenuData");
-            if(!oriObj) {
-                var dim = this.getElementDimension(self);
+            if(!oriObj || opt.force) {
+                var dim = opt.dimension ? opt.dimension : this.getElementDimension(self);
                 oriObj = {
-                    height: dim.height,
                     width: dim.width,
+                    height: dim.height,
                     fontSize: parseInt(self.css("font-size"))
                 };
                 self.data("waveMenuData", oriObj); // Save original
@@ -146,12 +191,13 @@
         },
 
         restoreScaleElementAndChildren: function(el, opt) {
-            var self = this;
-            var stop = opt.stop ? opt.stop : false;
-            //var stop = false;
-            self.scale(stop ? el.stop(true, true) : el, self.opts.beginningScale).children().each(function() {
-                self.scale(stop ? $(this).stop(true, true): $(this), self.opts.beginningScale);
-            });
+            if($(el).length) {
+                var self = this;
+                var stop = opt.stop ? opt.stop : false;
+                self.scale(stop ? el.stop(true, true) : el, self.opts.beginningScale).children().each(function() {
+                    self.scale(stop ? $(this).stop(true, true): $(this), self.opts.beginningScale);
+                });
+            }
         },
 
         // Restore Initial Scale
@@ -160,26 +206,183 @@
         },
 
         getElementDimension: function(el) {
-            /*if($.browser.msie || $.browser.mozilla || !el.is("img")) {
-                return {height: el.height(), width: el.width()};
+            var self = this;
+            //return {width: el.width(), height: el.height()};
+            return {width: self.actual($(el), 'width' ), height: self.actual($(el), 'height' )};
+        },
+
+        // Images Loaded tnks to Paul Irish
+        imagesLoaded: function($obj, callback ) {
+            var self = this,
+                $this = $obj,
+                deferred = $.isFunction($.Deferred) ? $.Deferred() : 0,
+                hasNotify = $.isFunction(deferred.notify),
+                $images = $this.find('img').add( $this.filter('img') ),
+                loaded = [],
+                proper = [],
+                broken = [];
+
+            function doneLoading() {
+                var $proper = $(proper),
+                    $broken = $(broken);
+
+                if ( deferred ) {
+                    if ( broken.length ) {
+                        deferred.reject( $images, $proper, $broken );
+                    } else {
+                        deferred.resolve( $images );
+                    }
+                }
+
+                if ( $.isFunction( callback ) ) {
+                    callback.call( $this, $images, $proper, $broken );
+                }
+            }
+
+            function imgLoaded( img, isBroken ) {
+                // don't proceed if BLANK image, or image is already loaded
+                if ( img.src === self.BLANK || $.inArray( img, loaded ) !== -1 ) {
+                    return;
+                }
+
+                // store element in loaded images array
+                loaded.push( img );
+
+                // keep track of broken and properly loaded images
+                if ( isBroken ) {
+                    broken.push( img );
+                } else {
+                    proper.push( img );
+                }
+
+                // cache image and its state for future calls
+                $.data( img, 'imagesLoaded', { isBroken: isBroken, src: img.src } );
+
+                // trigger deferred progress method if present
+                if ( hasNotify ) {
+                    deferred.notifyWith( $(img), [ isBroken, $images, $(proper), $(broken) ] );
+                }
+
+                // call doneLoading and clean listeners if all images are loaded
+                if ( $images.length === loaded.length ){
+                    setTimeout( doneLoading );
+                    $images.unbind( '.imagesLoaded' );
+                }
+            }
+
+            // if no images, trigger immediately
+            if ( !$images.length ) {
+                doneLoading();
             } else {
-                // Request the remote document
-                var dimension = {};
-                jQuery.ajax({
-                    url: el.attr("src"),
-                    type: "GET",
-                    dataType: "html",
-                    async: false,
-                    complete: function() {
-                        var ele = this.get(0);
-                        console.debug(this.outerHeight());
-                        dimension = {height: ele.outerHeight(), width: ele.outerWidth()};
-                    },
-                    context: $("<img/>")
-                });
-                return dimension;
-            }*/
-            return {height: el.height(), width: el.width()};
+                $images.bind( 'load.imagesLoaded error.imagesLoaded', function( event ){
+                    // trigger imgLoaded
+                    imgLoaded( event.target, event.type === 'error' );
+                }).each( function( i, el ) {
+                        var src = el.src;
+
+                        // find out if this image has been already checked for status
+                        // if it was, and src has not changed, call imgLoaded on it
+                        var cached = $.data( el, 'imagesLoaded' );
+                        if ( cached && cached.src === src ) {
+                            imgLoaded( el, cached.isBroken );
+                            return;
+                        }
+
+                        // if complete is true and browser supports natural sizes, try
+                        // to check for image status manually
+                        if ( el.complete && el.naturalWidth !== undefined ) {
+                            imgLoaded( el, el.naturalWidth === 0 || el.naturalHeight === 0 );
+                            return;
+                        }
+
+                        // cached images don't fire load sometimes, so we reset src, but only when
+                        // dealing with IE, or image is complete (loaded) and failed manual check
+                        // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
+                        if ( el.readyState || el.complete ) {
+                            el.src = self.BLANK;
+                            el.src = src;
+                        }
+                    });
+            }
+
+            return deferred ? deferred.promise( $this ) : $this;
+        },
+
+
+        // Actual dimension from ben-lin (ben). tnx!
+        actual : function(obj, method, options ){
+            var $hidden, $target, configs, css, tmp, actual, fix, restore;
+
+            $target = obj;
+
+            configs = $.extend({
+                absolute : false,
+                clone : false,
+                includeMargin : undefined
+            }, options );
+
+
+
+            if( configs.clone === true ){
+                fix = function(){
+                    // this is useful with css3pie
+                    $target = $target.filter( ':first' ).clone().css({
+                        position : 'absolute',
+                        top : -1000
+                    }).appendTo( 'body' );
+                };
+
+                restore = function(){
+                    // remove DOM element after getting the width
+                    $target.remove();
+                };
+            }else{
+                fix = function(){
+                    // get all hidden parents
+                    $hidden = $target.parents().andSelf().filter( ':hidden' );
+
+                    css = configs.absolute === true ?
+                    { position : 'absolute', visibility: 'hidden', display: 'block' } :
+                    { visibility: 'hidden', display: 'block' };
+
+                    tmp = [];
+
+                    // save the origin style props
+                    // set the hidden el css to be got the actual value later
+                    $hidden.each( function(){
+                        var _tmp = {}, name;
+                        for( name in css ){
+                            // save current style
+                            _tmp[ name ] = this.style[ name ];
+                            // set current style to proper css style
+                            this.style[ name ] = css[ name ];
+                        }
+                        tmp.push( _tmp );
+                    });
+                };
+
+                restore = function(){
+                    // restore origin style values
+                    $hidden.each( function( i ){
+                        var _tmp = tmp[ i ], name;
+                        for( name in css ){
+                            this.style[ name ] = _tmp[ name ];
+                        }
+                    });
+                };
+            }
+
+            fix();
+            // get the actual value with user specific methed
+            // it can be 'width', 'height', 'outerWidth', 'innerWidth'... etc
+            // configs.includeMargin only works for 'outerWidth' and 'outerHeight'
+            actual = /(outer)/g.test( method ) ?
+                $target[ method ]( configs.includeMargin ) :
+                $target[ method ]();
+
+            restore();
+            // IMPORTANT, this plugin only return the value of the first element
+            return actual;
         }
 	};
 
